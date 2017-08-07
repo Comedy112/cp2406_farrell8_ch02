b84e27509a29befa303f14fcf9890d29d0d0bc85
Heino <hendrik.nel@my.jcu.edu.au>
2017-08-07 13:39:24 +1000

    week 3 prac 2 Task 1 & 2 completed. (debugging exercises as well as programming exercises)

--- exercises/src/QuartsToGallonsInteractive.java
+++ exercises/src/QuartsToGallonsInteractive.java
@@ -1,5 +1,21 @@
 /**
  * Created by jc321687 on 30/08/16.
  */
-public class QuartsToGallonsInteractive {
+import java.util.Scanner;
+public class QuartsToGallonsInteractive
+{
+    public static void main(String[] args)
+    {
+        final int QUARTS_IN_GALLON = 4;
+        System.out.println("Please enter the number of quarts needed");
+        Scanner keyboard = new Scanner(System.in);
+        int quartsNeeded = keyboard.nextInt();
+        int gallonsNeeded;
+        int extraQuartsNeeded;
+        gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
+        extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
+        System.out.println("A job that needs " + quartsNeeded +
+                " quarts requires " + gallonsNeeded + " gallons plus " +
+                extraQuartsNeeded + " quarts.");
+    }
 }
